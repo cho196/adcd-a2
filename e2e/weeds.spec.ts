f5930235e9dcaee0cdca42a5073606ecfe11307f
cho196 <91933101+cho196@users.noreply.github.com>
2023-02-21 00:34:01 -0800

    fix tests

--- e2e/weeds.spec.ts
+++ e2e/weeds.spec.ts
@@ -10,13 +10,13 @@ test.describe("Table tests", () => {
     await page.goto(weedsPage);
     await expect(page.locator("th")).toHaveCount(5);
   });
-  test("Table vertical align", async ({ page }) => {
+  test("Table border collapse", async ({ page }) => {
     await page.goto(weedsPage);
     const table = page.locator("table");
     const grabbedDisplay = await table.evaluate((ele) => {
-      return window.getComputedStyle(ele).getPropertyValue("vertical-align");
+      return window.getComputedStyle(ele).getPropertyValue("border-collapse");
     });
-    expect(grabbedDisplay).toBe("top");
+    expect(grabbedDisplay).toBe("collapse");
   });
   test("Table font size", async ({ page }) => {
     await page.goto(weedsPage);
@@ -32,14 +32,14 @@ test.describe("Table tests", () => {
     const grabbedFontColor = await table.evaluate((ele) => {
       return window.getComputedStyle(ele).getPropertyValue("color");
     });
-    expect(grabbedFontColor).toBe("#000");
+    expect(grabbedFontColor).toBe("rgb(0, 0, 0)");
   });
-  test("Table border", async ({ page }) => {
+  test("Table margin", async ({ page }) => {
     await page.goto(weedsPage);
     const table = page.locator("table");
     const grabbedBorder = await table.evaluate((ele) => {
-      return window.getComputedStyle(ele).getPropertyValue("border");
+      return window.getComputedStyle(ele).getPropertyValue("margin");
     });
-    expect(grabbedBorder).toBe("1px solid #ccc");
+    expect(grabbedBorder).toBe("0px");
   });
 });
